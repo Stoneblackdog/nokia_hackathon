94a0effa412bc51846c47636e624ee37a086614e
Stoneblackdog <66771495+Stoneblackdog@users.noreply.github.com>
2024-05-24 23:48:59 +0200

    maze solver

--- maze_solver/main.py
+++ maze_solver/main.py
@@ -1,4 +1,22 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+#!/usr/bin/env python3
 
-print(input)
+from maze import solve_maze
+
+with open('./input.txt', 'r') as f:
+    input = f.read()
+    maze_a_and_b_c = input.strip().split('B')  # [A, BC]
+    maze_a = maze_a_and_b_c[0].replace('A', '')
+    maze_b = maze_a_and_b_c[1].split('C')[0]
+    maze_c = maze_a_and_b_c[1].split('C')[1]
+    solution_a = solve_maze(maze_a)
+    solution_b = solve_maze(maze_b)
+    solution_c = solve_maze(maze_c)
+
+    print('A')
+    print(*solution_a, sep=' ')
+    print()
+    print('B')
+    print(*solution_b, sep=' ')
+    print()
+    print('C')
+    print(*solution_c, sep=' ')
---
+++ maze_solver/maze.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self, ty):
+        self.up = None
+        self.down = None
+        self.left = None
+        self.right = None
+        self.ty = ty
+        self.visited = False
+        self.came_from = None
+
+
+def create_graph(maze: str):
+    # transform maze into more usable variant without spaces or newlines
+    maze = maze.replace(' ', '').split('\n')
+    maze = list(filter(None, maze))
+    # "swap" characters for nodes
+    node_matrix = [[Node(ty) for ty in line] for line in maze]
+    start = None
+
+    for y, line in enumerate(maze):
+        for x, char in enumerate(line):
+            if char == '#':
+                continue
+            if node_matrix[y - 1][x].ty != '#':
+                node_matrix[y][x].up = node_matrix[y - 1][x]
+            if node_matrix[y + 1][x].ty != '#':
+                node_matrix[y][x].down = node_matrix[y + 1][x]
+            if node_matrix[y][x - 1].ty != '#':
+                node_matrix[y][x].left = node_matrix[y][x - 1]
+            if node_matrix[y][x + 1].ty != '#':
+                node_matrix[y][x].right = node_matrix[y][x + 1]
+            if char == 'S':
+                start = node_matrix[y][x]
+
+    return start
+
+
+def find_destination_node(open_nodes: list):
+    new_open_nodes = []
+    while True:
+        for node in open_nodes:
+            if node.ty == 'G':
+                return node
+            if node.up is not None and node.up.visited is False:
+                node.up.came_from = node
+                new_open_nodes.append(node.up)
+            if node.down is not None and node.down.visited is False:
+                node.down.came_from = node
+                new_open_nodes.append(node.down)
+            if node.left is not None and node.left.visited is False:
+                node.left.came_from = node
+                new_open_nodes.append(node.left)
+            if node.right is not None and node.right.visited is False:
+                node.right.came_from = node
+                new_open_nodes.append(node.right)
+            node.visited = True
+        open_nodes = new_open_nodes
+
+
+def solve_maze(maze: str):
+    start = create_graph(maze)
+    solution = ['S']
+    stack = []
+    open_nodes = [start]
+
+    current_node = find_destination_node(open_nodes)
+    while True:
+        stack.append(current_node)
+        if current_node.ty == 'S':
+            break
+        current_node = current_node.came_from
+
+    while current_node.ty != 'G':
+        popped_node = stack.pop()
+        if popped_node == current_node.up:
+            solution.append('U')
+        elif popped_node == current_node.down:
+            solution.append('D')
+        elif popped_node == current_node.left:
+            solution.append('L')
+        elif popped_node == current_node.right:
+            solution.append('R')
+        current_node = popped_node
+
+    solution.append('G')
+    return solution
